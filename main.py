0509d5e5fea449bd1982e398ec9f68e257664c22
Vasilisa <277248801+Vasilisa-6-6@users.noreply.github.com>
2026-07-04 15:08:51 +0500

    +href

--- main.py
+++ main.py
@@ -17,7 +17,8 @@ def process_form():
     button_python = request.form.get('button_python')
     button_telega = request.form.get('button_telegram')
     button_html = request.form.get('button_html')
-    return render_template('index.html', button_python=button_python, button_telega=button_telega, button_html=button_html)
+    button_db = request.form.get('button_db')
+    return render_template('index.html', button_python=button_python, button_telega=button_telega, button_html=button_html, button_db=button_db)
 
 
 if __name__ == "__main__":
